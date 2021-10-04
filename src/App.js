34e7bd5cc9c9880b0bc76c03de0c3e957a5de624
upentest-demo <63858691+upentest-demo@users.noreply.github.com>
2021-10-04 12:29:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Howdy!! ---> </h1>
-      <h2>Is this a normal day?</h2>
-      <h2>I am deployig you. Hello from AWS!!</h2>
+      <h1>Howdy!!</h1>
+      <h2>Is this a normal day today? May be a quick demo??</h2>
+      <h2></h2>
     </div>
   );
 }
