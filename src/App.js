b2a14e6823119037635bef66342e74d6d5672ad1
upentest-demo <63858691+upentest-demo@users.noreply.github.com>
2021-10-07 20:59:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
       <h1>Howdy!!</h1>
       <h2>Is this a normal day today? May be a quick demo??</h2>
       <h2>Hey deploy this change please fo this demo!!</h2>
+      <h2>Full pipeline deploy</h2>
     </div>
   );
 }
