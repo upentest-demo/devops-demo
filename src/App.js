11936b12eeda3d312917e4eb206e932ff0bdfdc5
upentest-demo <63858691+upentest-demo@users.noreply.github.com>
2022-07-22 15:32:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <div>
       <h1>Howdy!!</h1>
-      <h2>Is this a normal day today? May be a quick demo??</h2>
       <h2>This is AWS Demo</h2>
     </div>
   );
