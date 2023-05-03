79a37b23e3341b5236b66dee1545d7efeaf3c75d
upentest-demo <63858691+upentest-demo@users.noreply.github.com>
2023-05-03 18:40:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Howdy, Ready for demo!</h1>
-      <h2>Lets trigger a build on 28-March-2023</h2>
+      <h2>Lets trigger a build!!</h2>
     </div>
   );
 }
