87b1d77af9dafdf1febf9068311b581f143cd438
upentest-demo <63858691+upentest-demo@users.noreply.github.com>
2023-08-22 17:19:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div>
       <h1>Howdy, How are you? </h1>
-      <h1>Let me deploay this </h1>
+      <h1>Let me deploy this </h1>
     
     </div>
   );
